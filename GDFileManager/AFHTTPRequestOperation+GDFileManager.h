9eeb6dec150d7e6097fdd0e74b8de379051a2310
Iuliia Zhelem <iuliia.zhelem@akvelon.com>
2016-06-22 00:15:08 +0300

    add new files (pod and configs) and files that were changed (AFNetworking) for Telegram project

---
+++ GDFileManager/AFHTTPRequestOperation+GDFileManager.h
@@ -0,0 +1,13 @@
+//
+//  AFHTTPRequestOperation+GDFileManager.h
+//  GDFileManagerExample
+//
+//  Created by Graham Dennis on 21/08/13.
+//  Copyright (c) 2013 Graham Dennis. All rights reserved.
+//
+
+#import "thirdparty/AFNetworking/TAFHTTPRequestOperation.h"
+
+@interface AFHTTPRequestOperation (GDFileManager)
+
+@end
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+
+target 'Telegraph' do
+    pod 'Lock', '~> 1.24'
+end
+
